2c70171b3374a8f4d6bbd6ba4df6c8de774130fa
GraydonE <124379361+GraydonE@users.noreply.github.com>
2023-04-18 14:57:15 +0000

    finished code (i thikn)

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+// Created by: Graydon Ezzeddin
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+"use strict";
+
+  // This function Calculates if it is warm or cold. 
+  function calculate() {
+    
+  // input random number and program feedback
+  let temperature = parseInt(document.getElementById('temperature').value);
+
+  let programFeedback = "" ;
+
+  if (temperature > 15) {
+    programFeedback = "It is cold";
+  }
+
+  if (userNumber <= 15) {
+    programFeedback = "It is warm";
+  }
+  
+  // output if its warm or cold
+  document.getElementById('program-feedback').innerHTML = programFeedback ;
+  
+}
